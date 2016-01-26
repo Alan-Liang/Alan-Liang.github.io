8f0ec6f60c01dd0acff0f96ee191163bbc37a749
Alan-Liang <yalun_liang@sina.com>
2016-01-26 15:26:14 +0800

    Update_20160126_1

---
+++ js/bdtb.js
@@ -0,0 +1,11 @@
+﻿var _open=window.open;//呵呵
+window.open=function(a,b){}
+
+function e(){
+var list=document.getElementsByTagName('textarea')[0].
+         value.split('\n');
+if(list[0]==undefined){alert(1);return false;}
+for(name in list){
+_open("redir.html?qiandaoapi#"+list[name],"newwindow"+list[name]);}
+return false;
+}
