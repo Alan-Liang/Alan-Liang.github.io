efd9839fe0ee23193144d0ac9430caaee7149bf9
Alan-Liang <yalun_liang@sina.com>
2016-01-31 04:51:35 +0800

    Update_20160131_1

--- js/bdtb.js
+++ js/bdtb.js
@@ -6,6 +6,6 @@ var list=document.getElementsByTagName('textarea')[0].
          value.split('\n');
 if(list[0]==undefined){alert(1);return false;}
 for(name in list){
-_open("redir.html?qiandaoapi#"+list[name],"newwindow"+list[name]);}
+_open("alan/redir.html?qiandaoapi#"+list[name],"newwindow"+list[name]);}
 return false;
 }
