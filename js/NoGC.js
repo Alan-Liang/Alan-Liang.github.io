eaa30f376a7c2f8765d7025b468fe2da1bb18800
Geoffrey, Tang <jeffswt@outlook.com>
2017-04-17 12:49:35 +0800

    你们给我搞的这个兼容性啊，Naive！

--- js/NoGC.js
+++ js/NoGC.js
@@ -1,6 +1,6 @@
 ﻿function NoGC(){
 	 if ((navigator.userAgent.indexOf("Chrome") > -1)){
-	alert('本系统使用chrome会出bug，请更换浏览器！');
+	alert('本系统使用chrome会出bug，请更换网站！');
 	if(window.location.href.split("/")[4]!=undefined)
 	window.location.href = "http://alan.liangcn.tk/fkGoCh/"+window.location.href.split("/")[4];
 	else window.location.href="http://alan.liangcn.tk/fkGoCh/";
