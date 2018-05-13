22e277648ecd1d701360dfd2fa5f568108b48cdd
Alan?Liang <16379418+Alan-Liang@users.noreply.github.com>
2018-05-13 20:40:36 +0800

    Update NoGC.js

--- js/NoGC.js
+++ js/NoGC.js
@@ -1,6 +1,6 @@
-﻿function NoGC(){
+function NoGC(){
 	 if ((navigator.userAgent.indexOf("Chrome") > -1)){
-	alert('本系统使用chrome会出bug，请更换浏览器！');
+	alert('本系统使用chrome会出bug，请勿访问，欢迎帮我改正！');
 	if(window.location.href.split("/")[4]!=undefined)
 	window.location.href = "http://alan.liangcn.tk/fkGoCh/"+window.location.href.split("/")[4];
 	else window.location.href="http://alan.liangcn.tk/fkGoCh/";
